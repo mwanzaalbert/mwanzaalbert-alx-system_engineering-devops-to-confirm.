ecfd7cf93b7956896b98bbad03fd6ffe156059f4
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-28 22:40:28 +0300

    Updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# installing flask using puppet from pip3
+# 1-install_a_package.pp
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0" && /usr/bin/pip3 show werkzeug | grep -q "Version: 2.0.3"',
+  require => Package['python3-pip'],
 }
