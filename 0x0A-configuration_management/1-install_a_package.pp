745499e2edcce1ccba84773a52550f3b9423661e
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-28 22:05:55 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # installing flask using puppet from pip3
-
-package { 'flask':
-  ensure   => '2.1.0',
+package { 'python3-pip':
+  ensure => installed,
   provider => 'pip3',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
