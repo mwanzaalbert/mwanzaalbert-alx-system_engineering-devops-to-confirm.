f0cd71b311319b727e1821e205fd400472b7349b
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-28 22:35:10 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,8 @@
 # 1-install_a_package.pp
 
 package { 'python3-pip':
-<<<<<<< HEAD
   ensure   => installed,
   provider => 'pip3',
-=======
-  ensure => installed,
->>>>>>> 4cd5cf94aca6a1e93c76036eaac00a25deda1f8a
 }
 
 exec { 'install_flask':
@@ -15,11 +11,8 @@ exec { 'install_flask':
   require => Package['python3-pip'],
 }
 
-<<<<<<< HEAD
-=======
 exec { 'create_symlink_for_flask':
   command => '/usr/bin/ln -sf /usr/local/bin/flask /usr/bin/flask',
   unless  => '/usr/bin/test -L /usr/bin/flask',
   require => Exec['install_flask'],
 }
->>>>>>> 4cd5cf94aca6a1e93c76036eaac00a25deda1f8a
