eea2395bf4ac4a137c427069b56bb85f34a32668
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2024-06-29 08:52:08 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,30 +1,11 @@
-# Ensure the ssh directory exists
-file { '~/.ssh':
-  ensure => directory,
-  mode   => '0700',
-  owner  => $USER,
-  group  => $USER,
+#configure ssh config
+
+file_line{'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
 }
 
-# Set the IdentityFile in SSH config
-file_line { 'Declare identity file':
-  path  => '~/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^IdentityFile',
+file_line{'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school'
 }
-
-# Refuse to authenticate using a password
-file_line { 'Turn off passwd auth':
-  path  => '~/.ssh/config',
-  line  => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication',
-}
-
-# Ensure the config file has the correct permissions
-file { '~/.ssh/config':
-  ensure => file,
-  mode   => '0600',
-  owner  => $USER,
-  group  => $USER,
-}
-
