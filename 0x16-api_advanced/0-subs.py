968ce04cc3065b3bb4f5d74a814aed7207dd7caa
mwanzaalbert <user.email>
2024-08-13 12:31:40 +0300

    undo changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,23 +14,14 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     # Check if the request was successful and not redirected
-    if response.status_code == 404:
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json().get('data', {})
+        # Return the number of subscribers
+        return data.get('subscribers', 0)
+    else:
         # Return 0 if the subreddit is invalid or redirected
         return 0
-    # Parse the JSON response
-    data = response.json().get('data', {})
-    # Return the number of subscribers
-    return data.get('subscribers', 0)
-
-    # # Check if the request was successful and not redirected
-    # if response.status_code == 200:
-    #     # Parse the JSON response
-    #     data = response.json().get('data', {})
-    #     # Return the number of subscribers
-    #     return data.get('subscribers', 0)
-    # else:
-    #     # Return 0 if the subreddit is invalid or redirected
-    #     return 0
 
 
 if __name__ == "__main__":
